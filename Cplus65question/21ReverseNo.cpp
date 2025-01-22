06c6d97916090a8dbaabdabc63bc221fcd70b836
gitritvik <ritvikrai789@gmail.com>
2025-01-22 20:36:42 +0530

    Finding revrese of a no.

--- Cplus65question/21ReverseNo.cpp
+++ Cplus65question/21ReverseNo.cpp
@@ -6,9 +6,21 @@ using namespace std;
 
 int main(){
 
+    int N, rev= 0;
+
+    cout<<"Enter the of which you want the Reverse- ";
+    cin>>N;
 
     
 
+    while(N != 0){
+        rev= rev + (N%10);
+        rev= rev*10;
+        N= N/10;
+    }
+
+    cout<<"Revrse is- "<<rev/10;
 
     return 0;
 }
+
