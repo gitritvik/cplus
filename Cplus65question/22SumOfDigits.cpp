8c177c5a7b73640f196aa9a795232e9fcdac7d9a
gitritvik <ritvikrai789@gmail.com>
2025-01-22 21:03:42 +0530

    Discription

--- Cplus65question/22SumOfDigits.cpp
+++ Cplus65question/22SumOfDigits.cpp
@@ -1,6 +1,16 @@
 // 9) Write a Program to display sum of all digits of a given
 // Number N by user
 
+// Input:
+//       - Enter the Number of which you want the Sum Of Digit.-  ; 
+        //input- 5678
+
+// Output:
+//       - It will print the Sum Of Digit of the number.
+        //Output- 26
+// Usage:
+//       Compile the program and run it. Enter the number as prompted.
+
 
 #include <iostream>
 using namespace std;
