a8e79c1acb33239cc1055c81ec64809c58d320c3
gitritvik <ritvikrai789@gmail.com>
2025-01-26 21:50:24 +0530

    New folder for OOPS Concept in C++

---
+++ Cplus65question/tempCodeRunnerFile.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int num1, num2;
+
+    cout<<"Enter the First limit- ";
+    cin>>num1;
+    cout<<"Enter the second limit- ";
+    cin>>num2;
+
+    for(num1; num1<=num2; num1++){
+        for(int j=2; j<= num1/2; j++){
+            
+            if(num1%j==0){
+
+            }
+           
+        }
+        
+    }
+
+    
+        
+
+
+
+
+
+
+
+    return 0;
+}
