7f1ed5a6f9c009002d2b9549bc517ba9f088908f
gitritvik <ritvikrai789@gmail.com>
2025-03-07 12:30:44 +0530

    Program to find factors of given no.

---
+++ Cplus65question/29FacatorsOfNum.cpp
@@ -0,0 +1,34 @@
+// 16) Write a Program to Display all Factors of a Number
+// entered by User
+
+// Input:
+//       Enter an number for which you want the Factors.
+        //input- 60
+
+// Output:
+//       - It will print all the factors of a given no.
+        //Factors of 60 are: 1 2 3 4 5 6 10 12 15 20 30 60
+// Usage:
+//       Compile the program and run it. Enter the as prompted.
+
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num; 
+
+    cout<<"Enter the no. of Which you want the Factor- ";
+    cin>>num;
+
+    for(int i; i<num; i++){
+        if(i)
+    }
+
+    
+
+
+    return 0;
+}
