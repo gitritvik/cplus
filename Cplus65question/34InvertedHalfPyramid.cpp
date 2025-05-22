f1e822fdea946ee8b84b3c741bfb1cbc248427b8
gitritvik <ritvikrai789@gmail.com>
2025-05-22 22:31:01 +0530

    Programm to print half intverted pyramid

---
+++ Cplus65question/34InvertedHalfPyramid.cpp
@@ -0,0 +1,42 @@
+//  4) Inverted Half pyramid
+//  * * * * * *
+//  * * * * *
+//  * * * *
+//  * * *
+//  * *
+//  *
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int i, j, h, m;
+
+    cout<<"Enter the hight of the Pramid- ";
+    cin>>h;
+    m=h;
+
+    cout<<"Your Half Inverted Pyramid is ready here- "<<endl;
+    
+    // I to print the no. star in the first row equals to the height of the pyramid
+    // then decrease one by one 
+    // for that I took another variable, 
+    // 
+
+    for(i=1; i<= h; i++){
+        for(j=1; j<=m; j++){
+            cout<<" * ";
+        }
+
+       
+
+        cout<<endl;
+         m=m-1;
+
+        
+    }
+
+    return 0;
+}
