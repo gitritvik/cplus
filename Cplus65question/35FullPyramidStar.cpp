a4870aa6f3a3dfd03c5ebf40cc0ba026c9494d3f
gitritvik <ritvikrai789@gmail.com>
2025-05-23 22:45:55 +0530

    corrected

--- Cplus65question/35FullPyramidStar.cpp
+++ Cplus65question/35FullPyramidStar.cpp
@@ -5,3 +5,21 @@
 //    * * * *
 //   * * * * *
 //  * * * * * *
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int i,j, h;
+
+    for(i=1; i<=h; i++){
+        for(j=1; j<=h; j++){
+            
+        }
+    }
+
+
+    return 0;
+}
